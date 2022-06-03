d54e2bbe973221a2b3c5b88c17863ecae65c9fe0
Jefferson Murillo <development@eticsas.co>
2022-06-03 16:59:49 -0500

    Fix: seccion_14 terminada correctamente

--- models/server.js
+++ models/server.js
@@ -1,14 +1,17 @@
 const express = require('express')
-const cors = require('cors')
+const cors = require('cors');
+
+const { socketController } = require('../sockets/controller');
 /* const morgan = require("morgan"); */
 
 
 class Server {
 
     constructor() {
-        this.app       = express()
-        this.port      = process.env.PORT
-
+        this.app       = express();
+        this.port      = process.env.PORT;
+        this.server    = require('http').createServer( this.app );
+        this.io        = require('socket.io')( this.server );
         this.paths = {};
         
 
@@ -17,6 +20,10 @@ class Server {
 
         // Rutas de mi aplicación
         this.routes()
+
+        // Sockets Config
+        this.sockets();
+
     }
 
 
@@ -38,8 +45,13 @@ class Server {
         /* this.app.use(this.paths.auth,       require('../routes/auth.routes')) */
     };
 
+    sockets() {
+
+        this.io.on("connection", socketController);
+    }
+
     listen() {
-        this.app.listen( this.port, () => {
+        this.server.listen( this.port, () => {
             console.log('Servidor corriendo en el puerto: ', this.port );
         })
     }
